47d9d5e7cd865a7d08925f7c74159d0c437d8ec7
arpithasureshappa <arpitha.sureshappa@tarento.com>
2024-07-22 09:13:22 +0530

    content partner updated

--- src/main/java/com/igot/cb/pores/util/Constants.java
+++ src/main/java/com/igot/cb/pores/util/Constants.java
@@ -310,6 +310,8 @@ public class Constants {
     public static final String EXCEPTION_MSG_UPDATE = "Exception occurred while updating record to ";
     public static final String FRAMEWORK = "framework";
     public static final String CHANNELS = "channels";
+    public static final String CONTENT_PROVIDER_INDEX_NAME = "content_provider";
+    public static final String CONTENT_PARTNER = "contentPartner";
     private Constants() {
     }
 }
